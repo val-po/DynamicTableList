8e44bd51c19e280a5f62addc4e0cb0ee461ad007
val-po <valentine.potapov@gmail.com>
2023-05-10 16:25:56 +0300

    Initial Commit

---
+++ DynamicTableList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DynamicTableList
+//
+//  Created by Val Po on 10.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DynamicTableList/DynamicTableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTableListApp.swift
+//  DynamicTableList
+//
+//  Created by Val Po on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
